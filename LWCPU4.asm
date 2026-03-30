65234cce7b8acc9500481a16490794295fcd8015
minecartchris <130516132+minecartchris@users.noreply.github.com>
2026-03-30 13:21:28 -0500

    fixed iinstrction set

--- LWCPU4.asm
+++ LWCPU4.asm
@@ -1,16 +1,16 @@
 #subruledef reg {
-    a   => 1`2
-    b   => 2`2
-    ST  => 3`2
+    a   => 0b01
+    b   => 0b10
+    ST  => 0b11
 }
 
 #subruledef cond {
-    C   => 0x1
-    Z   => 0x2
-    N   => 0x4
-    NC  => 0x9
-    NZ  => 0xa
-    NN  => 0xc
+    c   => 0b0001
+    z   => 0b0010
+    n   => 0b0100
+    nc  => 0b1001
+    nz  => 0b1010
+    nn  => 0b1100
 }
 
 #subruledef segment {
@@ -20,8 +20,7 @@
 
 #ruledef {
     nop => 0x00
-    hlt => 0x01
-    rst => 0x02
+    spc {op1: reg}  => 0x0 @ op1 @ 0b00
 
     jmp {addr: u8}  => 0x10 @ addr
     jmp {op1: reg}  => 0x1 @ op1 @ 0b00
@@ -44,39 +43,46 @@
     mov {seg: segment}, {op1: reg}  => 0x6 @ reg @ seg
     
     add {op1: reg}, {op2: reg}  => 0x7 @ op1 @ op2
-    add {op1: reg}, {data: i16} => 0x7 @ op1 @ 0b00 @ data
-    add {data: i16}, {op2: reg} => 0x7 @ 0b00 @ op2 @ data
+    add {op1: reg}, {data: i8}  => 0x7 @ op1 @ 0b00 @ data
+    add {data: i8}, {op2: reg}  => 0x7 @ 0b00 @ op2 @ data
     
     adc {op1: reg}, {op2: reg}  => 0x8 @ op1 @ op2
-    adc {op1: reg}, {data: i16} => 0x8 @ op1 @ 0b00 @ data
-    adc {data: i16}, {op2: reg} => 0x8 @ 0b00 @ op2 @ data
+    adc {op1: reg}, {data: i8}  => 0x8 @ op1 @ 0b00 @ data
+    adc {data: i8}, {op2: reg}  => 0x8 @ 0b00 @ op2 @ data
     
     sub {op1: reg}, {op2: reg}  => 0x9 @ op1 @ op2
-    sub {op1: reg}, {data: i16} => 0x9 @ op1 @ 0b00 @ data
-    sub {data: i16}, {op2: reg} => 0x9 @ 0b00 @ op2 @ data
+    sub {op1: reg}, {data: i8}  => 0x9 @ op1 @ 0b00 @ data
+    sub {data: i8}, {op2: reg}  => 0x9 @ 0b00 @ op2 @ data
     
     sbc {op1: reg}, {op2: reg}  => 0xa @ op1 @ op2
-    sbc {op1: reg}, {data: i16} => 0xa @ op1 @ 0b00 @ data
-    sbc {data: i16}, {op2: reg} => 0xa @ 0b00 @ op2 @ data
+    sbc {op1: reg}, {data: i8}  => 0xa @ op1 @ 0b00 @ data
+    sbc {data: i8}, {op2: reg}  => 0xa @ 0b00 @ op2 @ data
     
     cmp {op1: reg}, {op2: reg}  => 0xb @ op1 @ op2
-    cmp {op1: reg}, {data: i16} => 0xb @ op1 @ 0b00 @ data
-    cmp {data: i16}, {op2: reg} => 0xb @ 0b00 @ op2 @ data
+    cmp {op1: reg}, {data: i8}  => 0xb @ op1 @ 0b00 @ data
+    cmp {data: i8}, {op2: reg}  => 0xb @ 0b00 @ op2 @ data
     
     and {op1: reg}, {op2: reg}  => 0xc @ op1 @ op2
-    and {op1: reg}, {data: i16} => 0xc @ op1 @ 0b00 @ data
-    and {data: i16}, {op2: reg} => 0xc @ 0b00 @ op2 @ data
+    and {op1: reg}, {data: i8}  => 0xc @ op1 @ 0b00 @ data
+    and {data: i8}, {op2: reg}  => 0xc @ 0b00 @ op2 @ data
     
     or {op1: reg}, {op2: reg}   => 0xd @ op1 @ op2
-    or {op1: reg}, {data: i16}  => 0xd @ op1 @ 0b00 @ data
-    or {data: i16}, {op2: reg}  => 0xd @ 0b00 @ op2 @ data
+    or {op1: reg}, {data: i8}   => 0xd @ op1 @ 0b00 @ data
+    or {data: i8}, {op2: reg}   => 0xd @ 0b00 @ op2 @ data
     
     xor {op1: reg}, {op2: reg}  => 0xe @ op1 @ op2
-    xor {op1: reg}, {data: i16} => 0xe @ op1 @ 0b00 @ data
-    xor {data: i16}, {op2: reg} => 0xe @ 0b00 @ op2 @ data
+    xor {op1: reg}, {data: i8}  => 0xe @ op1 @ 0b00 @ data
+    xor {data: i8}, {op2: reg}  => 0xe @ 0b00 @ op2 @ data
     
-    shl {op1: reg}  => 0xf @ op1 @ 0b00
-    shr {op1: reg}  => 0xf @ op1 @ 0b01
-    rol {op1: reg}  => 0xf @ op1 @ 0b10
-    ror {op1: reg}  => 0xf @ op1 @ 0b11
+    shr {op1: reg}  => 0xf @ op1 @ 0b00
+    ror {op1: reg}  => 0xf @ op1 @ 0b01
+    asr {op1: reg}  => 0xf @ op1 @ 0b10
+    
+    shl {op1}   => asm {
+        add {op1}, {op1}
+    }
+    
+    rol {op1}   => asm {
+        adc {op1}, {op1}
+    }
 }
